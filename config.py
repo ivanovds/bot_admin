c8eef95345caafc28c61ae94b526e5420189b330
vitaliy <1999kvo@gmail.com>
2021-07-05 21:59:29 +0300

    debug receiving quantity

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ FEEDBACK_CHAT_ID = os.environ.get("FEEDBACK_CHAT_ID")
 UA_BOT_URL_HEROKU = os.environ.get("UA_BOT_URL_HEROKU", None)  # PROD bot
 UA_BOT_TOKEN = os.environ.get("UA_BOT_TOKEN")
 UA_BOT_HEROKU_API_KEY = os.environ.get("UA_BOT_HEROKU_API_KEY")
-UA_BOT_MAIN_PROCESS = os.environ.get("UA_BOT_HEROKU_API_KEY", "web")
+UA_BOT_MAIN_PROCESS = os.environ.get("UA_BOT_MAIN_PROCESS", "web")
 
 
 MAX_PENDING_UPDATE_COUNT = int(os.environ.get("MAX_PENDING_UPDATE_COUNT", 5))
