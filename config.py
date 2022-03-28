6adada8a91c0807606af515358e85544f9c467f9
ivanovds <sd16@pinesoftware.com.cy>
2022-03-28 13:06:23 +0400

    Bearer Authorization added

--- config.py
+++ config.py
@@ -8,6 +8,7 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN")
 APP_URL_HEROKU = os.environ.get("APP_URL_HEROKU")
 ENVIRONMENT = os.environ.get("ENVIRONMENT", None)  # "DEV"/"TEST"/"PROD"
 LANDING_URL_HEROKU = os.environ.get("LANDING_URL_HEROKU", 'https://corona-travel-bot.herokuapp.com/')
+API_KEY = os.environ.get("API_KEY", None)
 
 # DBP = f"host={os.environ.get("DB_HOST")} dbname={os.environ.get("DB_NAME")} user={os.environ.get("DB_USER")}" \
 #       f" password={os.environ.get("DB_PASSWORD")}  connect_timeout=3"
